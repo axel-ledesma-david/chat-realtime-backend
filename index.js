15ce345dd824f09495d107fbcd67413fd19591e3
axel-ledesma-david <axeldavidled999@gmail.com>
2023-07-14 01:06:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const { Server } = require("socket.io")
+const cors = require("cors")
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+})
+
+
+app.use(cors())
+
+
+io.on('connection', socket =>{
+    console.log(socket.id)
+    console.log('user conected')
+
+    socket.on("join_room", data => {
+        socket.join(data)
+        console.log(`use id: ${socket.id} joined room: ${data}`)
+    })
+
+    socket.on("send_message", data =>{
+        console.log(data)
+        socket.to(data.room).emit( "receive_message" ,data)
+    })
+})
+
+
+server.listen(process.env.PORT || 3001, ()=>{
+    console.log("Server on port 3001")
+})
